9a8548614fa5f2663e287e294fa02cca31c90564
LorAutumn <61251850+LorAutumn@users.noreply.github.com>
2020-07-28 21:33:52 +0200

    added eslint to project

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,8 +4,8 @@ import productsData from './products/productsData'
 
 // a list of all products that can be ordered
 function ProductList() {
-  // maps productData with ProductObject and props
-  const productComponents = productsData.map(product => <ProductObject name={product.name} price={product.price} />)
+  // maps productData with ProductObject and props -> returns an array of Objects
+  const productComponents = productsData.map(product => <ProductObject id={product.id} name={product.name} price={product.price} />)
   
   return (
     <div>
