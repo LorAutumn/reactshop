08ea8ccbe18485914897517ca3d85bf757159cd2
LorAutumn <61251850+LorAutumn@users.noreply.github.com>
2020-07-31 22:56:56 +0200

    added log in / log out state to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 // displays the page header with h1 and navbar (class based)
 class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: true
+    }
+  }
+  
   render() {
     return (
       <div className='header'>
         <h1>Shop Home</h1>
+        <p id='isLoggedIn'>{this.state.isLoggedIn ? 'log out' : 'log in'}</p>
         <nav>
           <ul className='nav'>
             <li className='home'>Home</li>
