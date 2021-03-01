bfe8c5bc8015e78f60e149d3017d9536c17e6207
61251850 <LorAutumn@users.noreply.github.com>
2021-03-01 22:59:05 +0100

    added product id routing for productDetailsPage on server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ import data from './productsData.js'
 
 const app = express()
 
+app.get('/api/products/:id', (req, res) => {
+    const productId = req.params.id
+    const product = data.products.find(x=>x.id === productId)
+    if(product)
+        res.send(product)
+    else
+        res.status(404).send({msg: 'Product Not Found.'})
+})
+
 app.get('/api/products', (req, res) => {
     res.send(data.products)
 })
