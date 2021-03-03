33a48e62fd4d3a9d8f78edf070d182c205cc4a4f
61251850 <LorAutumn@users.noreply.github.com>
2021-03-03 22:31:30 +0100

    ProductDetailsPage now renders id, name, and price of the acutal clicked product

--- Frontend/src/components/ProductDetailsPage.js
+++ Frontend/src/components/ProductDetailsPage.js
@@ -1,15 +1,28 @@
-import React, {useContext} from 'react'
-import {DataContext} from '../App'
+import React, { useContext, useEffect, useState } from 'react'
+import { DataContext } from '../App'
 
-function ProductsDetailsPage () {
+function ProductsDetailsPage() {
+    const productRouteId = window.location.pathname.slice(1)
     const dataContext = useContext(DataContext)
     const data = dataContext.data
-    console.log('data', data)
+    const [product, setProduct] = useState('hi')
 
-    return(
+    // sets the details of the actual clicked product as product state
+    useEffect(() => {
+        setProduct(
+            dataContext.data.find(x => x.id === parseInt(productRouteId))
+        )
+    }, [data])
+
+    // renders the product details page if product exists
+    return !product ? (
+        <div>loading...</div>
+    ) : (
         <div>
             <h1>ProductDetails</h1>
-            <p>{data.name}</p>
+            <p>{product.id}</p>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
         </div>
     )
 }
